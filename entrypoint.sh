2b8f65cb16bc0ad3218f54f19966cdca911e808a
Andrew Johnson <blackandred@users.noreply.github.com>
2019-04-01 07:09:46 +0200

    Fixed crontab on Debian / armhf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# depending on operating system, create an entrypoint for cron
+echo "#!/bin/bash" > /entrypoint.cron.sh
+
+if which crond > /dev/null; then
+    echo "crond -d 2 -f" >> /entrypoint.cron.sh
+else
+    echo "cron -f" >> /entrypoint.cron.sh
+fi
+
+chmod +x /entrypoint.cron.sh
+
 # allow to pass custom arguments from docker run command
 echo "#!/bin/bash" > /entrypoint.cmd.sh
 echo "infracheck --server --server-port 8000 $@" >> /entrypoint.cmd.sh
