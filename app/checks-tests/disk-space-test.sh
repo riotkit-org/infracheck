969d6a1d9ea70b5566b9c6f63fb058286715d14a
Andrew Johnson <blackandred@users.noreply.github.com>
2019-04-14 13:59:24 +0200

    Replaced call to "bc", as it works differently on different operating systems. The floats support in bash is not so good.

--- app/checks-tests/disk-space-test.sh
+++ app/checks-tests/disk-space-test.sh
@@ -11,3 +11,5 @@ MOCK_DF_OUTPUT="4GB" DIR=/ MIN_REQ_SPACE=4.5 ../checks/disk-space
 echo " >> Assert 5GB is enough, when minimum is 10GB"
 MOCK_DF_OUTPUT="5GB" DIR=/ MIN_REQ_SPACE=10 ../checks/disk-space
 [[ "$?" == 1 ]] && echo " .. Test failed." && exit 1
+
+exit 0
