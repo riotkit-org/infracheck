8ee36f32890d14b8decca492e6bbaba38c71f7cc
Michał Kern <9kp0rt@gmail.com>
2019-05-11 00:21:08 +0200

    fix disk-space-test.sh and add a mock value for checks/disk-space

--- app/checks-tests/disk-space-test.sh
+++ app/checks-tests/disk-space-test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo " >> Assert that 3,2GB is not enough, 4GB at least required"
-MOCK_DF_OUTPUT="3,2GB" DIR=/ MIN_REQ_SPACE=4 ../checks/disk-space
+MOCK_FREE_SPACE="3.2" DIR=/ MIN_REQ_SPACE=4 ../checks/disk_space.py
 [[ "$?" == 0 ]] && echo " .. Test failed." && exit 1
 
-echo " >> Assert 4GB is enough, when minimum is 4.5GB"
-MOCK_DF_OUTPUT="4GB" DIR=/ MIN_REQ_SPACE=4.5 ../checks/disk-space
+echo " >> Assert 4.5GB is enough, when minimum is 4GB"
+MOCK_FREE_SPACE="4.5" DIR=/ MIN_REQ_SPACE=4 ../checks/disk_space.py
 [[ "$?" == 1 ]] && echo " .. Test failed." && exit 1
 
-echo " >> Assert 5GB is enough, when minimum is 10GB"
-MOCK_DF_OUTPUT="5GB" DIR=/ MIN_REQ_SPACE=10 ../checks/disk-space
+echo " >> Assert 50GB is enough, when minimum is 10GB"
+MOCK_FREE_SPACE="50" DIR=/ MIN_REQ_SPACE=10 ../checks/disk_space.py
 [[ "$?" == 1 ]] && echo " .. Test failed." && exit 1
 
 exit 0
