1bf5a6e03cecf73e3d0d9524cb7b99b2bbcda0f1
Michał Kern <9kp0rt@gmail.com>
2019-05-11 00:24:12 +0200

    update disk-space-test.sh to run check with proper name

--- app/checks-tests/disk-space-test.sh
+++ app/checks-tests/disk-space-test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo " >> Assert that 3,2GB is not enough, 4GB at least required"
-MOCK_FREE_SPACE="3.2" DIR=/ MIN_REQ_SPACE=4 ../checks/disk_space.py
+MOCK_FREE_SPACE="3.2" DIR=/ MIN_REQ_SPACE=4 ../checks/disk-space
 [[ "$?" == 0 ]] && echo " .. Test failed." && exit 1
 
 echo " >> Assert 4.5GB is enough, when minimum is 4GB"
-MOCK_FREE_SPACE="4.5" DIR=/ MIN_REQ_SPACE=4 ../checks/disk_space.py
+MOCK_FREE_SPACE="4.5" DIR=/ MIN_REQ_SPACE=4 ../checks/disk-space
 [[ "$?" == 1 ]] && echo " .. Test failed." && exit 1
 
 echo " >> Assert 50GB is enough, when minimum is 10GB"
-MOCK_FREE_SPACE="50" DIR=/ MIN_REQ_SPACE=10 ../checks/disk_space.py
+MOCK_FREE_SPACE="50" DIR=/ MIN_REQ_SPACE=10 ../checks/disk-space
 [[ "$?" == 1 ]] && echo " .. Test failed." && exit 1
 
 exit 0
