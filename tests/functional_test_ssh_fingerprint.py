fd2c9d19bd8b26011749167c7cfd5678e9f06870
Andrew Johnson <blackandred@users.noreply.github.com>
2019-10-26 20:44:09 +0200

    Add `ssh-fingerprint` check: `Verifies if remote host fingerprint matches. Helps detecting man-in-the-middle and server takeover attacks.`

---
+++ tests/functional_test_ssh_fingerprint.py
@@ -0,0 +1,50 @@
+import unittest
+import docker
+
+from .utils import run_check
+from .utils.ssh_test import TestThatRequiresSshServer
+
+
+class SshFingerprintTest(TestThatRequiresSshServer, unittest.TestCase):
+    docker_client: docker.DockerClient
+
+    def test_success_case(self):
+        stdout: str
+        result: int
+
+        # we create SSH server for testing dynamically in a docker container, so each time it has a different identity
+        current_expected_fingerprint = self.get_current_ssh_server_fingerprint()
+
+        stdout, result, hooks_output = run_check('ssh-fingerprint', {
+            'HOST': 'localhost',
+            'PORT': 3222,
+            'EXPECTED_FINGERPRINT': current_expected_fingerprint.decode('utf-8')
+        }, {})
+
+        self.assertEqual('Fingerprint is OK', stdout.strip())
+        self.assertTrue(result)
+
+    def test_invalid_fingerprint(self):
+        stdout: str
+        result: int
+
+        stdout, result, hooks_output = run_check('ssh-fingerprint', {
+            'HOST': 'localhost',
+            'PORT': 3222,
+            'EXPECTED_FINGERPRINT': 'SOME FINGERPRINT THAT DOES NOT MATCH SERVER FINGERPRINT'
+        }, {})
+
+        self.assertIn('Fingerprint does not match', stdout.strip())
+        self.assertFalse(result)
+
+    def test_missing_host_parameter(self):
+        stdout: str
+        result: int
+
+        stdout, result, hooks_output = run_check('ssh-fingerprint', {
+            'PORT': 3222,
+            'EXPECTED_FINGERPRINT': 'SOME FINGERPRINT THAT DOES NOT MATCH SERVER FINGERPRINT'
+        }, {})
+
+        self.assertIn('You need to provide a HOST', stdout.strip())
+        self.assertFalse(result)
