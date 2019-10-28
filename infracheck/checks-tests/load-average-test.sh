3a76afcfd8f9d6e956c35cdc3128b474036cd856
Andrew Johnson <blackandred@users.noreply.github.com>
2019-10-28 06:59:06 +0100

    Tests: Corrected paths in tests

--- infracheck/checks-tests/load-average-test.sh
+++ infracheck/checks-tests/load-average-test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo " >> Assert that 1.5 core load is more than 1.0 core allowed"
-MAX_LOAD=1.0 MOCK_LOAD_AVERAGE=1.5 ./infracheck/checks/load-average
+MAX_LOAD=1.0 MOCK_LOAD_AVERAGE=1.5 ../checks/load-average
 if [[ $? != 1 ]]; then
     echo " >> Test failed."
     exit 1
 fi
 
 echo " >> Assert that 1.5 core load is more than 1.0 core allowed (using integer instead of float)"
-MAX_LOAD=1 MOCK_LOAD_AVERAGE=1.5 ./infracheck/checks/load-average
+MAX_LOAD=1 MOCK_LOAD_AVERAGE=1.5 ../checks/load-average
 if [[ $? != 1 ]]; then
     echo " >> Test failed."
     exit 1
 fi
 
 echo " >> Assert that load of 10.1 is ok, when we allow load of max. 15.0"
-MAX_LOAD=15 MOCK_LOAD_AVERAGE=10.1 ./infracheck/checks/load-average
+MAX_LOAD=15 MOCK_LOAD_AVERAGE=10.1 ../checks/load-average
 if [[ $? != 0 ]]; then
     echo " >> Test failed."
     exit 1
--- infracheck/checks-tests/swap-usage-max-percent-test.sh
+++ infracheck/checks-tests/swap-usage-max-percent-test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo " >> Assert that current usage of 50% is higher than maximum of 40%"
-MOCK_SWAP_USAGE=50 MAX_ALLOWED_PERCENTAGE=40 ./infracheck/checks/swap-usage-max-percent
+MOCK_SWAP_USAGE=50 MAX_ALLOWED_PERCENTAGE=40 ../checks/swap-usage-max-percent
 if [[ $? != 1 ]]; then
     echo " >> Test failed."
     exit 1
 fi
 
 echo " >> Assert that swap is not used"
-MOCK_SWAP_USAGE=0 MAX_ALLOWED_PERCENTAGE=0 ./infracheck/checks/swap-usage-max-percent
+MOCK_SWAP_USAGE=0 MAX_ALLOWED_PERCENTAGE=0 ../checks/swap-usage-max-percent
 if [[ $? != 0 ]]; then
     echo " >> Test failed."
     exit 1
 fi
 
 echo " >> Assert 10% is ok, when maximum is defined at 15%"
-MOCK_SWAP_USAGE=10 MAX_ALLOWED_PERCENTAGE=15 ./infracheck/checks/swap-usage-max-percent
+MOCK_SWAP_USAGE=10 MAX_ALLOWED_PERCENTAGE=15 ../checks/swap-usage-max-percent
 if [[ $? != 0 ]]; then
     echo " >> Test failed."
     exit 1
