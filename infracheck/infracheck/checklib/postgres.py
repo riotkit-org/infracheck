6dac168a029b8d06e2e47e1962ce404bfb686a0f
Andrew Johnson <blackandred@users.noreply.github.com>
2019-11-30 10:50:03 +0100

    Added PostgreSQL replication checks

---
+++ infracheck/infracheck/checklib/postgres.py
@@ -0,0 +1,55 @@
+
+import psycopg2
+import psycopg2.extras
+
+
+class BasePostgreSQL:
+    conn = None
+
+    def __init__(self, host: str, dbname: str, port: int, username: str, password: str, connect_timeout: int):
+        self.conn = psycopg2.connect(
+            database=dbname,
+            host=host,
+            password=password,
+            user=username,
+            port=port,
+            connect_timeout=connect_timeout
+        )
+
+    @staticmethod
+    def create_instance(cls):
+        return cls(
+            host=os.getenv('DB_HOST', 'localhost'),
+            dbname=os.getenv('DB_NAME', ''),
+            username=os.getenv('DB_USER', 'root'),
+            password=os.getenv('DB_PASSWORD', ''),
+            connect_timeout=int(os.getenv('DB_CONNECT_TIMEOUT', 10))
+        )
+
+    def query(self, qstr: str):
+        cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(qstr)
+
+        return cur.fetchall()
+
+    def validate_replication_row_exists(self, sql: str, expected_status: str, expected_user: str):
+        out = self.query(sql)
+        active_replications = len(out)
+
+        for row in out:
+            status, conn_info = row
+
+            if status != expected_status and expected_user in conn_info:
+                return False, 'Expected "%s" status for conn_info="%s", got "%s"' % (
+                    expected_status, conn_info, status
+                )
+
+            if status == expected_status and expected_user in conn_info:
+                return True, "%i replications active. The replication for '%s' user looks healthy" % (
+                    active_replications, expected_user
+                )
+
+        if active_replications == 0:
+            return False, "no replications active"
+
+        return False, "%i replications active, but none found for user '%s'" % (active_replications, expected_user)
